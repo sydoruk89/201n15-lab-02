1b1730bb6a7c904d95e30b916b011a305c439882
Roman Sydoruk <sydoruk89@gmail.com>
2019-08-16 23:10:15 -0700

    Lab 3

--- README.md
+++ README.md
@@ -2,5 +2,8 @@ Lab 2
 "About me" project
 Roman Sydoruk 201N15
 Links and Resources
+	www.facebook.com
+	www.instagram.com
+	www.linkedin.com
 
 
--- js/app.js
+++ js/app.js
@@ -1,79 +1,86 @@
 'use strict';
 
-
+var correct = 0;
 // Ask users name
 
 var usersName = prompt('Hello! What\'s your name?');
 console.log('User\'s name is ' + usersName);
 alert('Can you guess the correct answers about me?');
 
-//First question
-var likeWeather = prompt('Do I like the weather in Seattle? Yes/No');
-likeWeather = likeWeather.toLowerCase();
-if(likeWeather === 'yes'  || likeWeather === 'y'){
-  alert('You are not right! ');
-  console.log('The answer is', likeWeather);
-} else if (likeWeather === 'no' || likeWeather === 'n') {
-  alert('Yep, that is true, I do not like the weather in Seattle but I like the city and the nature around!');
-  console.log('The answer is', likeWeather);
-}
-else {
-  alert('What do you mean?!');
-}
 
-//Second question
+// Question 1-5
 
-var howOld = prompt('2. Am I 30 years old? Yes/No');
-howOld = howOld.toLowerCase();
-if(howOld === 'yes'  || howOld === 'y') {
-  alert ('Yes, that is correct! Good job!');
-  console.log('The answer is', howOld);
-} else if (howOld === 'no' || howOld === 'n') {
-  alert('Nope!');
-  console.log('The answer is', howOld);
-}
-else { 
-  alert('What do you mean?!');
+var questions = ['1. Do I like the weather in Seattle?', '2. Am I 30 years old?', '3. Have I ever been to Thailand?', '4. Have I lived somewhere else in the USA besides WA?', '5. Do I like Mediterranean cuisine?'];
+var yesNo = ['yes', 'y', 'no', 'n'];
+for (var i = 0; i < 5; i++) {
+  var askQuestion = prompt(questions[i]);
+  askQuestion = askQuestion.toLowerCase();
+  if (i === 0) {
+    if (askQuestion === yesNo[2] || askQuestion === yesNo[3]){
+      alert('Correct!');
+      correct++;
+    } else {
+      alert('Nope!');
+    }
+  } else {
+    if (askQuestion === yesNo[0] || askQuestion === yesNo[1]){
+      alert('Correct!');
+      correct++;
+    } else {
+      alert('Nope!');
+    }
+  }
 }
 
-//Third question
 
-var beenToThailand = prompt('3. Have I ever been to Thailand? Yes/No');
-beenToThailand = beenToThailand.toLowerCase();
+// Sixth question
 
-if(beenToThailand === 'yes' || beenToThailand === 'y'){
-  alert('Yes, that is correct! Good job!');
-  console.log('The answer is', beenToThailand);
-} else if (beenToThailand === 'no' || beenToThailand === 'n') {
-  alert('Nope!');
-  console.log('The answer is', beenToThailand); 
-} else {
-  alert('What do you mean?!');
+for (var i = 0; i < 4; i++) {
+  var averageMiles = prompt('6. How many miles do I average walk every day? You\'ve got four chances to get it right!');
+  if (averageMiles === '2') {
+    alert('You are right!');
+    console.log('The answer is ' + averageMiles);
+    correct++;
+    break;
+  }
+  else if (averageMiles < '2') {
+    alert('Too low!');
+    console.log(averageMiles + ' is too low');
+  }
+  else {
+    alert('Too high!');
+    console.log(averageMiles + ' is to high');
+  }
 }
 
-//Fourth question
 
-var livedSomewhere = prompt('4. Have I lived somewhere else in the USA besides WA? Yes/No');
-livedSomewhere = livedSomewhere.toLowerCase();
-if (livedSomewhere === 'yes'  || livedSomewhere === 'y') {
-  alert('Yes, you are right! I haved lived in Maryland for abot 4 months 7 years ago!');
-  console.log('The answer is', livedSomewhere);
-} else if (livedSomewhere === 'no' || livedSomewhere === 'n') {
-  alert ('Nope!');
-  console.log('The answer is', livedSomewhere);
-} else  {
-  alert('What do you mean?!');}
+// Seventh question
 
-// Fifth question
-var likeCuisine = prompt ('5. Do I like Mediterranean cuisine?');
-likeCuisine = likeCuisine.toLowerCase();    
-if(likeCuisine === 'yes' || likeCuisine === 'y'){
-  alert('Yes, that is true! I like Italian, Spanish and Greek cuisine!');
-  console.log('The answer is', likeCuisine);
-} else if (likeCuisine === 'no' || likeCuisine === 'n'){
-  alert('Nope!');
-  console.log('The answer is', likeCuisine);
-} else {
-  alert('What do you mean?!'); }
+var guessSport = prompt ('7.What is one of my favourite kind of sport?');
+guessSport = guessSport.toLowerCase();
+var myFavouriteSport = ['soccer', 'tennis', 'volleyball'];
+var count = 1;
+var q7Guesses = 6;
+var inArray = false;
+while(count <= q7Guesses) {
+  var guessesLeft = q7Guesses - count;
+  count++;
 
+  for(i = 0; i < myFavouriteSport.length; i++) {
+    if(guessSport === myFavouriteSport[i]){
+      inArray = true;
+    }
+  }
+  if (inArray === true){
+    correct++;
+    alert('Yep!');
+    break;
+  }
+  else if (inArray === false){
+    guessSport = prompt('Sorry, not one of my favorite sports! Try again. You have ' + guessesLeft + ' guesses left!');
+  }
+}
+
+alert('You got ' + correct + ' correct answers.');
+console.log('correct answers ' + correct);
 
